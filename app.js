e53d177d8675f8dc51d17a57fc3c3807db29a443
bhogalj15 <jazz3806@gmail.com>
2023-10-20 17:35:11 -0400

    added funcitonality to restart game

--- app.js
+++ app.js
@@ -219,6 +219,7 @@ class Checkers {
     this.selectedPeice.dataset.col = target.dataset.col;
     this.checkKing();
     this.updateScoreUI();
+    this.winCondition();
   }
 
   //Validate capture
@@ -426,12 +427,30 @@ class Checkers {
   //Check win condition
   winCondition() {
     if(this.black === 0) {
-      alert("Player 1 has WON!");
+      showNotification("Player 1 has WON!");
+      this.resetGame();
       return;
     } else if(this.red === 0) {
-      alert("Player 2 has WON!");
+      showNotification("Player 2 has WON!");
+      this.resetGame();
       return;
     }
+  }
+
+  resetGame() {
+    this.clearBoard();
+    this.black = 12;
+    this.red = 12;
+    this.renderCheckerPeices();
+  }
+
+  clearBoard() {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+      if(square.firstElementChild) {
+        square.removeChild(square.firstElementChild);
+      }
+    })
   }
 }
 
@@ -439,84 +458,3 @@ document.addEventListener("DOMContentLoaded", () => {
   const newGame = new Checkers();
 });
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   renderBoard();
-//   renderDarkAndLightSquares();
-//   renderCheckerPeices();
-//   mouseHover();
-// });
-
-// const renderBoard = () => {
-//   const board = document.querySelector("#board");
-
-//   //Loop for each row
-//   for (let i = 0; i < 8; i++) {
-//     //Loop for each column in a row
-//     for (let j = 0; j < 8; j++) {
-//       const square = document.createElement("div");
-//       //Assigning classes and data atrributes
-//       square.classList.add("square");
-//       square.dataset.row = i;
-//       square.dataset.col = j;
-
-//       //Adding the square to the board
-//       //There will be a total of 64 square divs
-//       board.appendChild(square);
-//     }
-//   }
-//   console.log(board);
-// };
-
-// const renderCheckerPeices = () => {
-//   //Select board with child elements that have the class "sqaure"
-//   const squares = document.querySelectorAll("#board .square");
-
-//   squares.forEach((square) => {
-//     const row = parseInt(square.dataset.row);
-//     const col = parseInt(square.dataset.col);
-
-//     //Check if is the square is a dark square
-//     if ((row + col) % 2 === 0) {
-//       //Place player 1 peices
-//       if (row < 3) {
-//         const peice = document.createElement("div");
-//         peice.classList.add("peice", "player1");
-//         square.appendChild(peice);
-//       } else if (row > 4) {
-//         const peice = document.createElement("div");
-//         peice.classList.add("peice", "player2");
-//         square.appendChild(peice);
-//       }
-//     }
-//   });
-// };
-
-// const renderDarkAndLightSquares = () => {
-//   const squares = document.querySelectorAll("#board .square");
-//   squares.forEach((square) => {
-//     const row = parseInt(square.dataset.row);
-//     const col = parseInt(square.dataset.col);
-
-//     //Change square to dark
-//     if ((row + col) % 2 === 0) {
-//       square.classList.add("dark");
-//     } else {
-//       square.classList.add("light");
-//     }
-//   });
-// };
-
-// const mouseHover = () => {
-//   const squares = document.querySelectorAll("#board .square");
-
-//   squares.forEach((square) => {
-//     if (square.classList.contains("dark")) {
-//       square.addEventListener("mouseover", () => {
-//         square.classList.add("mouseHoverDark");
-//         square.addEventListener("mouseout", () => {
-//           square.classList.remove("mouseHoverDark");
-//         });
-//       });
-//     }
-//   });
-// };

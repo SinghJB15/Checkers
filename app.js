7ebd4e20675f0567e8912b98dcd6a14877aa36b9
bhogalj15 <jazz3806@gmail.com>
2023-10-20 02:17:07 -0400

    refactors app.js for better organization and readability

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 class Checkers {
   constructor() {
+    this.initVariables();
     this.renderBoard();
     this.renderDarkAndLightSquares();
     this.renderCheckerPeices();
     this.mouseHover();
     this.onBoardClick();
+  }
+
+  // Initialization Variables - Initial setup for gameplay
+  initVariables() {
     this.selectedPeice = null;
     this.black = 12;
     this.red = 12;
     this.currentTurn = "player2";
     this.currentPlayer = "black";
   }
-  //Initialization and Rendering - Methods related to setting up the game board.
+
+  // Initialization and Rendering
+
+  //Render the 8x8 board
   renderBoard() {
     const board = document.querySelector("#board");
-
     //Loop for each row
     for (let i = 0; i < 8; i++) {
       //Loop for each column in a row
@@ -33,6 +40,7 @@ class Checkers {
     console.log(board);
   }
 
+  //Render dark and light squares
   renderDarkAndLightSquares() {
     const squares = document.querySelectorAll("#board .square");
     squares.forEach((square) => {
@@ -48,6 +56,7 @@ class Checkers {
     });
   }
 
+  //Render black and red checker peices
   renderCheckerPeices() {
     //Select board with child elements that have the class "sqaure"
     const squares = document.querySelectorAll("#board .square");
@@ -76,7 +85,9 @@ class Checkers {
     });
   }
 
-  //Event Handling - Methods associated with event listeners and related responses
+  // Event Handling
+
+  // Highlighting the squares and peices
   mouseHover() {
     const squares = document.querySelectorAll("#board .square");
     const peices = document.querySelectorAll("#board .peice");
@@ -102,6 +113,7 @@ class Checkers {
     });
   }
 
+  //On board click
   onBoardClick() {
     const squares = document.querySelectorAll(".square");
     squares.forEach((square) => {
@@ -118,41 +130,62 @@ class Checkers {
     });
   }
 
+  //Handle clicks
   handleClick(event) {
+    const targetSquare = event.target;
+
     const { peiceRow, peiceCol, targetRow, targetCol } =
-      this.extractPositions(event);
+      this.extractPositions(targetSquare);
     //Validate target has no children elements (no checker peice)
 
     if (!this.selectedPeice.classList.contains(this.currentTurn)) {
       return alert("not your current turn");
     }
-    if (event.target.children.length !== 0) {
+    if (targetSquare.children.length !== 0) {
       return this.invalidMove();
     }
     //Normal checker move
     if (this.validateMove(peiceRow, peiceCol, targetRow, targetCol)) {
-      return this.executeMove(event.target);
+      this.executeMove(targetSquare);
+      this.selectedPeice = null; //nullify after the move
+      this.switchTurn();
+      return;
     }
     //Capture move validation
     if (Math.abs(targetRow - peiceRow) === 2) {
-      return this.captureMove(event);
+      if (this.handleCapture(targetSquare)) {
+        //If a capture has occured, then check for potential mulit-jumps
+        if (this.checkForMultiJumps(targetSquare)) {
+          //If mulit jump is possible, notify user
+          alert("Another Capture is possible!");
+          return; //do not switch turns
+        } else {
+          //no more captures are possible
+          this.switchTurn();
+          this.selectedPeice = null;
+          return; //exit function after handling multi jump
+        }
+      }
+    } else {
+      //invalid capture move
+      return this.invalidMove();
     }
+    // if above all fails
     return this.invalidMove();
   }
 
-  invalidMove() {
-    alert("This is an invalid move!");
-  }
+  // Move Logic
 
-  //Move Logic - Methods that handle the logic behind moving pieces
-  extractPositions(event) {
+  //Extract positions
+  extractPositions(targetSquare) {
     const peiceRow = parseInt(this.selectedPeice.dataset.row);
     const peiceCol = parseInt(this.selectedPeice.dataset.col);
-    const targetRow = parseInt(event.target.dataset.row);
-    const targetCol = parseInt(event.target.dataset.col);
+    const targetRow = parseInt(targetSquare.dataset.row);
+    const targetCol = parseInt(targetSquare.dataset.col);
     return { peiceRow, peiceCol, targetRow, targetCol };
   }
 
+  //Validate move
   validateMove(peiceRow, peiceCol, targetRow, targetCol) {
     //Check if a peice is a king
     if (this.selectedPeice.dataset.type === "king") {
@@ -179,22 +212,23 @@ class Checkers {
     }
   }
 
+  //Execute Move
   executeMove(target) {
     target.appendChild(this.selectedPeice);
     this.selectedPeice.dataset.row = target.dataset.row;
     this.selectedPeice.dataset.col = target.dataset.col;
     this.checkKing();
-    this.selectedPeice = null;
     this.updateScoreUI();
-    this.updateUI();
   }
 
-  captureMove(event) {
+  //Validate capture
+  validateCapture(targetSquare) {
     const { peiceRow, peiceCol, targetRow, targetCol } =
-      this.extractPositions(event);
+      this.extractPositions(targetSquare);
 
     //Determine direction of movement
-    let rowDirection, colDirection;
+    let rowDirection;
+    let colDirection;
     if (targetRow - peiceRow > 0) {
       rowDirection = 1;
     } else {
@@ -214,7 +248,7 @@ class Checkers {
     );
 
     if (!opponentSquare || !opponentSquare.firstElementChild) {
-      return this.invalidMove();
+      return false; //no capture possible
     }
 
     const opponentPeice = opponentSquare.firstElementChild;
@@ -222,8 +256,8 @@ class Checkers {
     //Handle King's movement and capturing
     if (
       this.selectedPeice.dataset.type === "king" &&
-      (targetRow === peiceRow - 2 || targetRow === peiceRow + 2) &&
-      (targetCol === peiceCol - 2 || targetCol === peiceCol + 2)
+      (targetRow === peiceRow + 2 || targetRow === peiceRow - 2) &&
+      (targetCol === peiceCol + 2 || targetCol === peiceCol - 2)
     ) {
       if (
         (this.selectedPeice.classList.contains("player1") &&
@@ -231,11 +265,10 @@ class Checkers {
         (this.selectedPeice.classList.contains("player2") &&
           opponentPeice.classList.contains("player1"))
       ) {
-        this.capturePeice(opponentSquare, opponentPeice);
-        this.executeMove(event.target);
-        return;
+        return true;
       }
     }
+
     //Handle Regular Movements
     if (
       this.selectedPeice.classList.contains("player1") &&
@@ -243,9 +276,7 @@ class Checkers {
       (targetCol === peiceCol + 2 || targetCol === peiceCol - 2) &&
       opponentPeice.classList.contains("player2")
     ) {
-      this.capturePeice(opponentSquare, opponentPeice);
-      this.executeMove(event.target);
-      return;
+      return true;
     }
 
     if (
@@ -254,14 +285,85 @@ class Checkers {
       (targetCol === peiceCol + 2 || targetCol === peiceCol - 2) &&
       opponentPeice.classList.contains("player1")
     ) {
-      this.capturePeice(opponentSquare, opponentPeice);
-      this.executeMove(event.target);
-      return;
+      return true;
     }
-
-    this.invalidMove();
+    //if above all fails return false indicating capture move is not possible
+    return false;
   }
 
+  //Handle capture
+  handleCapture(targetSquare) {
+    if (this.validateCapture(targetSquare)) {
+      const { peiceRow, peiceCol, targetRow, targetCol } =
+        this.extractPositions(targetSquare);
+
+      //Determine direction of movement
+      let rowDirection;
+      let colDirection;
+
+      if (targetRow - peiceRow > 0) {
+        rowDirection = 1;
+      } else {
+        rowDirection = -1;
+      }
+      if (targetCol - peiceCol > 0) {
+        colDirection = 1;
+      } else {
+        colDirection = -1;
+      }
+
+      //Determine Opponent's square
+      const opponentRow = peiceRow + rowDirection;
+      const opponentCol = peiceCol + colDirection;
+      const opponentSquare = document.querySelector(
+        `[data-row='${opponentRow}'][data-col='${opponentCol}']`
+      );
+
+      const opponentPeice = opponentSquare.firstElementChild;
+
+      this.capturePeice(opponentSquare, opponentPeice);
+      this.executeMove(targetSquare);
+      return true;
+    }
+    return false;
+  }
+
+  //Check for multi jumps
+  checkForMultiJumps(currentSquare) {
+    const directions = [
+      { row: 2, col: 2 },
+      { row: 2, col: -2 },
+      { row: -2, col: 2 },
+      { row: -2, col: -2 },
+    ];
+
+    const peiceRow = parseInt(currentSquare.dataset.row);
+    const peiceCol = parseInt(currentSquare.dataset.col);
+
+    for (const direction of directions) {
+      const targetRow = peiceRow + direction.row;
+      const targetCol = peiceCol + direction.col;
+      const targetSquare = document.querySelector(
+        `[data-row='${targetRow}'][data-col='${targetCol}']`
+      );
+
+      //ensure target square is on the board
+      if (!targetSquare) {
+        continue; //if targetSquare is null or undefined, do not proceed with rest of the code and skip to the next iteration
+      }
+
+      if (
+        !targetSquare.firstElementChild &&
+        this.validateCapture(targetSquare)
+      ) {
+        return true;
+      }
+    }
+    //if above all fails
+    return false;
+  }
+
+  //Capture Peice
   capturePeice(opponentSquare, opponentPeice) {
     if (opponentPeice.classList.contains("player1")) {
       this.red--;
@@ -271,7 +373,9 @@ class Checkers {
     opponentSquare.removeChild(opponentPeice);
   }
 
-  //Gameplay Utilities - methods that update the UI
+  // Gameplay Utilities
+
+  //Check king
   checkKing() {
     if (this.selectedPeice.classList.contains("player2")) {
       if (this.selectedPeice.dataset.row === "0") {
@@ -290,13 +394,15 @@ class Checkers {
     }
   }
 
+  //Update UI
   updateScoreUI() {
     document.querySelector(
       "#captures"
     ).innerHTML = `Red: ${this.red} | Black: ${this.black}`;
   }
 
-  updateUI() {
+  //Switch turns
+  switchTurn() {
     if (this.currentPlayer === "black") {
       this.currentPlayer = "red";
       this.currentTurn = "player1";
@@ -310,6 +416,11 @@ class Checkers {
         "#current-player"
       ).innerHTML = `Player: ${this.currentPlayer}`;
     }
+  }
+
+  //Invalid moves
+  invalidMove() {
+    alert("This is an invalid move");
   }
 }
 
